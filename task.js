d6d94558fa5ddb3710c268bbe1e4f26cd45b3599
Flatiron School <s.lin12@gmail.com>
2017-11-29 17:08:10 -0500

    done

--- task.js
+++ task.js
@@ -1,42 +1,87 @@
-function seeLists() {
-  fetch('http://localhost:3000/lists').then(res => res.json()).then(json => seeListData(json))
+const store = {}
+
+function populateStore() {
+  fetch('http://localhost:3000/lists').then(res => res.json()).then(json => addListsToStore(json))
 }
 
-function seeListData(json) {
-  let ul = document.createElement('ul');
-  json.forEach(function(list){
-    let div = document.createElement('div')
-    div.id = `div-${list.id}`
-    let button = document.createElement('button')
-    button.id = list.id;
-    button.className = "task-button";
-    button.innerText = "See Tasks";
-    div.appendChild(button)
-    ul.innerHTML +=  `<li> ${list.name}</li>`;
-    ul.appendChild(div);
-  })
-  let lists = document.querySelector('.lists')
-  lists.appendChild(ul);
+function addListsToStore(json) {
+  store["lists"] = json
+}
 
+function seeListData() {
+  let lists = document.querySelector('.lists')
+  if (lists.innerText === "") {
+    let json = store.lists
+    let ul = document.querySelector('.lists-ul');
+    json.forEach(function(list){
+      let div = document.createElement('div')
+      div.id = `div-${list.id}`
+      let button = document.createElement('button')
+      button.id = list.id;
+      button.className = "task-button";
+      button.innerText = "See Tasks";
+      div.appendChild(button)
+      let li = document.createElement('li')
+      li.innerText = list.name
+      li.append(div)
+      ul.appendChild(li);
+    })
+
+    lists.appendChild(ul);
+  }
 }
 
 document.addEventListener('click', function(e){
   if (e.target.classList.contains('task-button')){
-    return seeTasks(e.target.id);
+    return seeTasksData(e.target.id);
   }
 })
 
-function seeTasks(id){
-  url = "http://localhost:3000/lists/" + id
-  fetch(url).then(res => res.json()).then(json => seeTasksData(json))
-}
-
-function seeTasksData(json){
-  const tasks = json.tasks
+function seeTasksData(id){
+  const list = store.lists.find(function(list) {
+    return list.id === parseInt(id);
+  })
+  let tasks = list.tasks
   let ul = document.createElement('ul')
   tasks.forEach(function(task) {
     ul.innerHTML += `<li> Name: ${task.name} <br>Completed: ${task.completed}</li>`
   })
-  let button = document.getElementById(`div-${json.id}`)
+  let button = document.getElementById(`div-${id}`)
   button.append(ul);
 }
+
+function createList() {
+  let input = document.querySelector(".list-name").value
+  let url = "http://localhost:3000/lists/"
+
+  fetch(url, {
+    method: 'post',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({"list": {"name": input}})
+  }).then(res => res.json()).then(json => addNewList(json))
+
+  document.querySelector(".list-name").value = ""
+}
+
+function addNewList(json) {
+  store.lists.push(json);
+  let ul = document.querySelector('.lists-ul');
+  let newList = document.createElement('li');
+
+  if (ul.innerText != "") {
+    let div = document.createElement('div')
+    div.id = `div-${json.id}`
+    let button = document.createElement('button')
+    button.id = json.id;
+    button.className = "task-button";
+    button.innerText = "See Tasks";
+    div.appendChild(button)
+    let li = document.createElement('li')
+    li.innerText = json.name
+    li.append(div)
+    ul.appendChild(li);
+  }
+
+}
+
+populateStore()
